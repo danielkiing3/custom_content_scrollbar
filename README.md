c70f370ce0af3478db724a301bc7f68e1ea07ef5
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-05-07 03:22:09 +0100

    Initial build

---
+++ README.md
@@ -0,0 +1,3 @@
+# custom_content_scrollbar
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/custom_content_scrollbar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.custom_content_scrollbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
