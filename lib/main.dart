6c181da971e076dfb0e70ae807bb7247d692d9ea
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-05-11 21:25:37 +0100

    feat: Initialize main application with ArticleScreen and dummy data

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'data/dummy_article.dart';
+import 'article_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+  @override
+  State<MainApp> createState() => _MainApp();
+}
+
+class _MainApp extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: ArticleScreen(article: dummyArticle));
+  }
+}
