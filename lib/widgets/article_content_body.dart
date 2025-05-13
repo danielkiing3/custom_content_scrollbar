452ed04e35462dbcfd7365bf354d206184312566
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-05-13 21:44:02 +0100

    feat: Add ArticleContentList widget for displaying article content with scrollable subcontent

---
+++ lib/widgets/article_content_body.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../models/article_subcontent.dart';
+import '../utils/constants/breakpoint.dart';
+import '../utils/theme/app_text_styles.dart';
+import 'article_subcontent_widget.dart';
+
+class ArticleContentList extends StatelessWidget {
+  const ArticleContentList({
+    super.key,
+    required this.scrollController,
+    required this.subContentKeys,
+    required this.articleTitle,
+    required this.subContent,
+  });
+
+  final ScrollController scrollController;
+  final String articleTitle;
+  final List<ArticleSubcontent> subContent;
+  final List<GlobalKey> subContentKeys;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: kMaxContentWidth),
+        child: SingleChildScrollView(
+          controller: scrollController,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                // -- Title
+                Text(articleTitle, style: AppTextStyles.heading),
+                const SizedBox(height: 36),
+
+                // -- Sub content
+                ...List<Widget>.generate(subContent.length, (int i) {
+                  return ArticleSubcontentWidget(contentKey: subContentKeys[i], articleSubcontent: subContent[i]);
+                }),
+                const SizedBox(height: 300),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
